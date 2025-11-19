25cdbaf7cb5532b1ed87e2ab4a5bf8252a705a9a
Ercan SORMAZ <ercansormaz@gmail.com>
2025-11-19 10:36:39 +0300

    Latest version to 8.4.0

--- docker-build.sh
+++ docker-build.sh
@@ -4,4 +4,5 @@ docker buildx build --platform linux/amd64,linux/arm64 -f 6.2/Dockerfile --push 
 docker buildx build --platform linux/amd64,linux/arm64 -f 7.2/Dockerfile --push -t ercansormaz/redis-cluster:7.2.12 .
 docker buildx build --platform linux/amd64,linux/arm64 -f 7.4/Dockerfile --push -t ercansormaz/redis-cluster:7.4.7 .
 docker buildx build --platform linux/amd64,linux/arm64 -f 8.0/Dockerfile --push -t ercansormaz/redis-cluster:8.0.5 .
-docker buildx build --platform linux/amd64,linux/arm64 --push -t ercansormaz/redis-cluster:8.2.3 -t ercansormaz/redis-cluster:latest .
+docker buildx build --platform linux/amd64,linux/arm64 -f 8.2/Dockerfile --push -t ercansormaz/redis-cluster:8.2.3 .
+docker buildx build --platform linux/amd64,linux/arm64 --push -t ercansormaz/redis-cluster:8.4.0 -t ercansormaz/redis-cluster:latest .
