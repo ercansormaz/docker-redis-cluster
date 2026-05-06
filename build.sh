7789af7c51616698e1600491a958953db0be7fb6
Ercan SORMAZ <ercansormaz@gmail.com>
2026-05-06 17:15:17 +0300

    Latest versions to 6.2.22, 7.2.14, 7.4.9, 8.2.6, 8.4.3, 8.6.3

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker buildx build --platform linux/amd64,linux/arm64 -f 6.2/Dockerfile --push -t ercansormaz/redis-cluster:6.2.21 -t ercansormaz/redis-cluster:6.2 .
-docker buildx build --platform linux/amd64,linux/arm64 -f 7.2/Dockerfile --push -t ercansormaz/redis-cluster:7.2.13 -t ercansormaz/redis-cluster:7.2 .
-docker buildx build --platform linux/amd64,linux/arm64 -f 7.4/Dockerfile --push -t ercansormaz/redis-cluster:7.4.8 -t ercansormaz/redis-cluster:7.4 .
+docker buildx build --platform linux/amd64,linux/arm64 -f 6.2/Dockerfile --push -t ercansormaz/redis-cluster:6.2.22 -t ercansormaz/redis-cluster:6.2 .
+docker buildx build --platform linux/amd64,linux/arm64 -f 7.2/Dockerfile --push -t ercansormaz/redis-cluster:7.2.14 -t ercansormaz/redis-cluster:7.2 .
+docker buildx build --platform linux/amd64,linux/arm64 -f 7.4/Dockerfile --push -t ercansormaz/redis-cluster:7.4.9 -t ercansormaz/redis-cluster:7.4 .
 docker buildx build --platform linux/amd64,linux/arm64 -f 8.0/Dockerfile --push -t ercansormaz/redis-cluster:8.0.6 -t ercansormaz/redis-cluster:8.0 .
-docker buildx build --platform linux/amd64,linux/arm64 -f 8.2/Dockerfile --push -t ercansormaz/redis-cluster:8.2.5 -t ercansormaz/redis-cluster:8.2 .
-docker buildx build --platform linux/amd64,linux/arm64 -f 8.4/Dockerfile --push -t ercansormaz/redis-cluster:8.4.2 -t ercansormaz/redis-cluster:8.4 .
-docker buildx build --platform linux/amd64,linux/arm64 -f 8.6/Dockerfile --push -t ercansormaz/redis-cluster:8.6.2 -t ercansormaz/redis-cluster:8.6 -t ercansormaz/redis-cluster:latest .
+docker buildx build --platform linux/amd64,linux/arm64 -f 8.2/Dockerfile --push -t ercansormaz/redis-cluster:8.2.6 -t ercansormaz/redis-cluster:8.2 .
+docker buildx build --platform linux/amd64,linux/arm64 -f 8.4/Dockerfile --push -t ercansormaz/redis-cluster:8.4.3 -t ercansormaz/redis-cluster:8.4 .
+docker buildx build --platform linux/amd64,linux/arm64 -f 8.6/Dockerfile --push -t ercansormaz/redis-cluster:8.6.3 -t ercansormaz/redis-cluster:8.6 -t ercansormaz/redis-cluster:latest .
